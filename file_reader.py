81633e8a26e24bd6f93c399b70850805707fb832
NotDesp <nicholas.t.hirt@gmail.com>
2023-03-01 17:13:12 -0800

    Some clean up of files for more readability, light comment adding.

--- file_reader.py
+++ file_reader.py
@@ -1,9 +1,9 @@
 def file_read():
-    sample_data_file = open("MushroomData_8000.txt", "r")
-    sample_data_list = sample_data_file.readlines()
+    sample_data_file = open("MushroomData_8000.txt", "r") # Opens known mushroom file in read mode
+    sample_data_list = sample_data_file.readlines() # Reads each line into an array element
 
     for index, data in enumerate(sample_data_list):
-        data = data.strip("\n")
-        sample_data_list[index] = data.split(',')
+        data = data.strip("\n") # Trims new line from each index
+        sample_data_list[index] = data.split(',')   # Splits elements by ',' creating a 2d array
 
     return sample_data_list
--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import mushroom
 import file_reader
 
 sample_data_list = file_reader.file_read()
-mushroom_list = []
-for element in sample_data_list:
-    mushroom_list.append(mushroom.mushroom(element[0], element[1], element[2], element[3], element[4], element[5], element[6], element[7],
-                                  element[8], element[9], element[10], element[11], element[12], element[13], element[14],
-                                  element[15], element[16], element[17], element[18], element[19], element[20], element[21]))
+mushroom_list = mushroom.mushroom_list_create(sample_data_list)
 
 for mushroom in mushroom_list:
     print(mushroom.classes)
