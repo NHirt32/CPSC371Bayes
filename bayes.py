6aa6c1b9aef71c3e181466c038ff67a859125186
NHirt32 <nicholas.t.hirt@gmail.com>
2023-03-15 15:14:11 -0700

    Only thing we need now is file output and maybe some cleaning of unused code?

--- bayes.py
+++ bayes.py
@@ -3,11 +3,12 @@ def unknown_mushroom_list_probability(known_mushroom_list, unknown_mushroom_list
     for index in range(len(unknown_mushroom_list)):
         edibility_odds = (x_probability(known_mushroom_list,unknown_mushroom_list[index]) *
                           x_probability_given_edible(known_mushroom_list,unknown_mushroom_list[index]))/ edible_probability(known_mushroom_list)
-        if edibility_odds > 0.5:
-            #print("Edible mushroom detected")
+        poison_odds = (x_probability(known_mushroom_list, unknown_mushroom_list[index]) *
+                          x_probability_given_poisonous(known_mushroom_list,
+                                                     unknown_mushroom_list[index])) / poison_probability(known_mushroom_list)
+        if edibility_odds > poison_odds:
             unknown_mushroom_list[index][0] = 'e'
         else:
-            #print("Poisonous mushroom detected")
             unknown_mushroom_list[index][0] = 'p'
 
 
@@ -44,11 +45,35 @@ def x_probability_given_edible(known_mushroom_list, unknown_mushroom):
         attribute_position += 1
     return probability
 
+def x_probability_given_poisonous(known_mushroom_list, unknown_mushroom):
+    # Very similar to x_probability, however, before adding something to our shared attribute count we must also confirm that it is poisonous
+    # Debug print
+    probability = 1 # We start our probability at 100* so we can multiply by each discovered probability
+    mushroom_attribute_count = 22  # Variable to check the number of attributes our mushroom contains
+    attribute_position = 1 # We start at one to ignore the edibility column
+    while attribute_position < mushroom_attribute_count:    # We are going to iterate through every attribute of our unknown_mushroom
+        current_attribute = unknown_mushroom[attribute_position]
+        shared_attributes = 0
+        for mushrooms in known_mushroom_list:
+            if mushrooms[attribute_position] == current_attribute and mushrooms[0] == 'p':
+                shared_attributes += 1
+        shared_over_total = shared_attributes / len(known_mushroom_list)
+        probability = probability * shared_over_total  # P(A) AND P(B) = P(A) * P(B)
+        attribute_position += 1
+    return probability
 
 def edible_probability(known_mushroom_list):
     edible_count = 0
     for mushrooms in known_mushroom_list:
         if mushrooms[0] == 'e':
+            edible_count += 1
+    probability = edible_count/len(known_mushroom_list)
+    return probability
+
+def poison_probability(known_mushroom_list):
+    edible_count = 0
+    for mushrooms in known_mushroom_list:
+        if mushrooms[0] == 'p':
             edible_count += 1
     probability = edible_count/len(known_mushroom_list)
     return probability
